35401e8fe8a64918127099b047c825c691ea46f1
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-14 10:10:11 -0600

    Successfully created hashedpassword

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,8 @@ import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully!" });
