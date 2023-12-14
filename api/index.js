a2b9205d6bbb13c06e47e9b994cb98f9dcee80ab
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-14 09:05:19 -0600

    created app.get api test route

--- api/index.js
+++ api/index.js
@@ -17,3 +17,9 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!");
 });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Api is working!",
+  });
+});
