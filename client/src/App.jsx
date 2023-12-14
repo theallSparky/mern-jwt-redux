920a47a46810b3332471b8aacf7696f9fd0d5596
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-13 21:14:59 -0600

    Updated paths and and component imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIN";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
       </Routes>
     </BrowserRouter>
   );
