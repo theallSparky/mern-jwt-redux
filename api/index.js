6efa65eec15ddeb8b2d5cc078a02baba8c43b311
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-14 08:50:22 -0600

    Connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
