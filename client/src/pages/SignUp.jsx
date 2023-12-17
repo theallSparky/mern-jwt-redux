b3a3e5a2ef808758c678d5c3c43fc3a336fd8e5e
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-16 23:27:49 -0600

    Added useState, handleChange, handleSubmit in /signUp

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,27 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevents the page from reloading upon form submit
+    const res = await fetch("http://localhost:3000/api/auth/signup", formData);
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-75">
           Sign Up
