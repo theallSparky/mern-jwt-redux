dad5af60ba2cdc5b95f1ab5b09dd733ee4581019
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-17 22:48:34 -0600

    Started making userSlice.js

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    signInFailure: (state, action) => {
+      state.loading = true;
+      state.error = action.payload;
+    },
+  },
+});
