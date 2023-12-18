af95dbf7721d52cb26f8f1f7fcd3eab67a6781a1
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-17 21:07:26 -0600

    added signin post route, added res.cookies status and json(validUser)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,6 +10,10 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully!" });
+    res
+      .cookies("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(validUser); // httpOnly disllows 3rd party applications from modifying the cookie
   } catch (error) {
     next(error);
   }
@@ -22,6 +26,7 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
   } catch (error) {
     next(error);
   }
