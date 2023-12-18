92b9886292afe1a37629d6af52dfc2fd2c96476a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-17 21:28:34 -0600

    Finished creating signinapi route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,10 +27,12 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(validUser);
+      .json(rest);
   } catch (error) {
     next(error);
   }
