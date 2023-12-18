0b3236117e9517afdbf56e83ff1147beeddc8c14
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-17 22:12:42 -0600

    Removed the       console.log(data) from signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -21,7 +21,6 @@ export default function SignUp() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
       setLoading(false);
       if (data.success === false) {
         setError(true);
