263ef149f5c339f06170416744cd652daff7b335
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-17 20:18:04 -0600

    signin post route, const validPassword and logic for if !validPassword

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,6 +19,8 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
   } catch (error) {
     next(error);
   }
