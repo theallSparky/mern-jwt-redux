cfd473e7cd37b94d4b595f78e99a6fc7bf3cfc24
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-19 18:58:10 -0600

    Added OAuth to SignIn.jsx and SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ import {
   signInFailure,
 } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -69,6 +70,7 @@ export default function SignUp() {
         >
           {loading ? "Loading" : "Sign Up"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
