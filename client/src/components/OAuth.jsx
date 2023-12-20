99d5bc974f31b5e0a764cad94ee9bb3092e94200
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-19 19:01:40 -0600

    Changed OAuth type to button to prevent form submission

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function OAuth() {
   return (
-    <button className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95">
+    <button
+      type="button"
+      className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95"
+    >
       Continue with Google
     </button>
   );
