cc82a5001f1d43fed5839d4294db5b7b7aa0bfbe
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-19 18:01:23 -0600

    Added user to combineReducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 
+const rootReducer = combineReducers({ user: userReducer });
+
 export const store = configureStore({
   reducer: { user: userReducer },
   middleware: (getDefaultMiddleware) =>
