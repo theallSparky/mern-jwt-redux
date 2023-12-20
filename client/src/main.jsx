c3013616d97fe682d04b38698c6a68112185c43a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-19 18:18:01 -0600

    Deleted loading={null} in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate persistor={persistor} loading={null}>
+    <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
