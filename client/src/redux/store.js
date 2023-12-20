26a9578ba89f180963c3b3b1cf6e0e49f62b75a0
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-19 18:03:12 -0600

    imported persistReducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer } from 'redux-persist'
 
 const rootReducer = combineReducers({ user: userReducer });
+
+const persistedReducer = persistReducer
+
+}
 
 export const store = configureStore({
   reducer: { user: userReducer },
