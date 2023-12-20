f2e979e89df6bc35e515ac805042f2b7a1c14596
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-19 19:00:29 -0600

    Added and stylized continue with google button

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function OAuth() {
-  return <div>OAuth</div>;
+  return (
+    <button className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95">
+      Continue with Google
+    </button>
+  );
 }
