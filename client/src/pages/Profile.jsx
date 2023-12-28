dacaaa5c9cee88871121d86647725f5550364be4
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-28 16:04:33 -0600

    Started styling on profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,17 @@
+import { useSelector } from "react-redux";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div>
+      <h1 className="text-3xl font-weight-600 text-center my-7">Profile</h1>
+      <form className="flex flex-col">
+        <img
+          src={currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center cursor-pointer"
+        />
+      </form>
+    </div>
+  );
 }
