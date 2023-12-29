1f8d828bece695761474dd8b23435ccd9565106c
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-29 14:09:45 -0600

    Added and stylized first input on form on profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,13 +3,19 @@ import { useSelector } from "react-redux";
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-weight-600 text-center my-7">Profile</h1>
-      <form className="flex flex-col">
+      <form className="flex flex-col gap-4">
         <img
           src={currentUser.profilePicture}
           alt="profile"
           className="h-24 w-24 self-center cursor-pointer object-cover rounded-full"
+        />
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 rounded-l p-3"
         />
       </form>
     </div>
