6b2a88455d1d56df7d64597de792b3ff9593236d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-01 13:12:21 -0600

    Made jwt.verify logic

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,4 +4,11 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) return res.status(401).json({ error: "You need to login!" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json("Token is not valid");
+
+    req.user = user;
+    next();
+  });
 };
