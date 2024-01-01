cf49a23dc22fad1f203b973f4305a2c78da0edd5
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-01 13:08:39 -0600

    Made api/index.js (!token) cleaner by making it one line

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,7 +3,5 @@ import { Jwt } from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  if (!token) {
-    return res.status(401).json({ error: "You need to login!" });
-  }
+  if (!token) return res.status(401).json({ error: "You need to login!" });
 };
