043d60833709bbb390d4683ffc8d94698606d074
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-01 13:01:21 -0600

    Added (!token) logic and imported jwt

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,3 +1,9 @@
+import { Jwt } from "jsonwebtoken";
+
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
+
+  if (!token) {
+    return res.status(401).json({ error: "You need to login!" });
+  }
 };
