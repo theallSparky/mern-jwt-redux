a6447b0033629e24fcefef43684659ce06ff0901
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-02 19:24:41 -0600

    Fixed user.controller.js logic

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -29,7 +29,7 @@ export const updateUser = async (req, res, next) => {
       { new: true }
     );
     const { password, ...rest } = updatedUser._doc;
-    res.status(200).json(updatedUser);
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
